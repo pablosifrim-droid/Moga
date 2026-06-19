e59d395dfdb04229e676f4e9548b01aebe9395e3
Pablo Sifrim <pablo.sifrim@gmail.com>
2026-06-19 09:44:02 +0200

    Initial Commit

---
+++ Moga/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moga
+//
+//  Created by Pablo Sifrim on 19/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moga/MogaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogaApp.swift
+//  Moga
+//
+//  Created by Pablo Sifrim on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MogaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
